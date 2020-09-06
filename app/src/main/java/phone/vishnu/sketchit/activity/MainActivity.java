3e2e7902ff3d534d8fc8e45d39d5e69adf9311f5
VishnuSanal <t.v.s10123@gmail.com>
2020-09-06 23:42:58 +0530

    Changed Permission Asking

--- app/src/main/java/phone/vishnu/sketchit/activity/MainActivity.java
+++ app/src/main/java/phone/vishnu/sketchit/activity/MainActivity.java
@@ -8,7 +8,6 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
 import android.media.MediaScannerConnection;
 import android.os.AsyncTask;
 import android.os.Build;
@@ -16,10 +15,7 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.SeekBar;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -40,39 +36,6 @@ import phone.vishnu.sketchit.view.SketchView;
 public class MainActivity extends AppCompatActivity {
 
     private SketchView sketchView;
-    private AlertDialog.Builder currentAlertDialog;
-    private ImageView imageView;
-    private AlertDialog widthAlertDialog;
-    private ImageView colorChooser, strokeWidth, clearAll, saveAll;
-    private SeekBar.OnSeekBarChangeListener seekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
-
-        Bitmap bitmap = Bitmap.createBitmap(400, 100, Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(bitmap);
-
-        @Override
-        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
-            Paint p = new Paint();
-            p.setColor(sketchView.getDrawingColor());
-            p.setStrokeCap(Paint.Cap.ROUND);
-            p.setStrokeWidth(progress);
-
-            bitmap.eraseColor(Color.WHITE);
-            canvas.drawLine(30, 50, 370, 50, p);
-            imageView.setImageBitmap(bitmap);
-
-        }
-
-        @Override
-        public void onStartTrackingTouch(SeekBar seekBar) {
-
-        }
-
-        @Override
-        public void onStopTrackingTouch(SeekBar seekBar) {
-
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,10 +45,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         sketchView = findViewById(R.id.view);
 
-        colorChooser = findViewById(R.id.colorChooseIV);
-        strokeWidth = findViewById(R.id.strokeWidthIV);
-        clearAll = findViewById(R.id.clearAllIV);
-        saveAll = findViewById(R.id.saveIV);
+        ImageView colorChooser = findViewById(R.id.colorChooseIV);
+        final ImageView strokeWidth = findViewById(R.id.strokeWidthIV);
+        strokeWidth.setPadding((45 - 5) / 2, (45 - 5) / 2, (45 - 5) / 2, (45 - 5) / 2);
+        ImageView clearAll = findViewById(R.id.clearAllIV);
+        ImageView saveAll = findViewById(R.id.saveIV);
 
         colorChooser.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -97,7 +61,16 @@ public class MainActivity extends AppCompatActivity {
         strokeWidth.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showWidthAlertDialog();
+                int width = sketchView.getLineWidth();
+
+                if (width >= 45) {
+                    sketchView.setLineWidth(width = 5);
+                    strokeWidth.setPadding((45 - width) / 2, (45 - width) / 2, (45 - width) / 2, (45 - width) / 2);
+                } else {
+                    sketchView.setLineWidth(width += 10);
+                    strokeWidth.setPadding(width, width, width, width);
+                    strokeWidth.setPadding((45 - width) / 2, (45 - width) / 2, (45 - width) / 2, (45 - width) / 2);
+                }
             }
         });
 
@@ -111,7 +84,49 @@ public class MainActivity extends AppCompatActivity {
         saveAll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                requestPermission();
+                {
+                    if (!isPermissionGranted())
+                        isPermissionGranted();
+                    else {
+                        AsyncTask.execute(new Runnable() {
+                            @Override
+                            public void run() {
+
+                                if (!sketchView.isNull()) {
+
+                                    File root = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), "SketchIt");
+                                    if (!root.exists()) root.mkdirs();
+
+                                    SharedPreferences sharedPreferences = getSharedPreferences("phone.vishnu.statussaver", Context.MODE_PRIVATE);
+                                    int lastInt = (sharedPreferences.getInt("number", 0)) + 1;
+                                    String file = root.toString() + File.separator + "SketchIt" + lastInt + ".jpg";
+                                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                                    editor.putInt("number", lastInt);
+                                    editor.apply();
+
+                                    Bitmap result = Bitmap.createBitmap(sketchView.getWidth(), sketchView.getHeight(), Bitmap.Config.ARGB_8888);
+                                    Canvas c = new Canvas(result);
+                                    sketchView.draw(c);
+
+                                    try {
+                                        FileOutputStream fOutputStream = new FileOutputStream(file);
+                                        final BufferedOutputStream bos = new BufferedOutputStream(fOutputStream);
+
+                                        result.compress(Bitmap.CompressFormat.JPEG, 90, bos);
+
+                                        fOutputStream.flush();
+                                        fOutputStream.close();
+
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                    MediaScannerConnection.scanFile(MainActivity.this, new String[]{file}, null, null);
+
+                                }
+                            }
+                        });
+                    }
+                }
             }
         });
 
@@ -136,80 +151,46 @@ public class MainActivity extends AppCompatActivity {
                 .show();
     }
 
-    void showWidthAlertDialog() {
-
-        currentAlertDialog = new AlertDialog.Builder(this);
-
-        View view = getLayoutInflater().inflate(R.layout.set_width_dialog, null);
-        final SeekBar seekBar = view.findViewById(R.id.widthSeekBar);
-        Button button = view.findViewById(R.id.widthButton);
-        imageView = view.findViewById(R.id.widthImageView);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sketchView.setLineWidth(seekBar.getProgress());
-                widthAlertDialog.dismiss();
-                currentAlertDialog = null;
-            }
-
-        });
-
-        seekBar.setOnSeekBarChangeListener(seekBarChangeListener);
-        seekBar.setProgress(sketchView.getLineWidth());
-
-        currentAlertDialog.setView(view);
-        widthAlertDialog = currentAlertDialog.create();
-        widthAlertDialog.show();
-    }
-
-    private void requestPermission() {
+    private boolean isPermissionGranted() {
         if (Build.VERSION.SDK_INT >= 22) {
             if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
                 if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-                    Toast.makeText(MainActivity.this, "Please Accept Required Permission", Toast.LENGTH_SHORT).show();
+                    showPermissionDeniedDialog();
+                } else {
+                    int PERMISSION_REQ_CODE = 2;
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQ_CODE);
                 }
-                int PERMISSION_REQ_CODE = 222;
-                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQ_CODE);
             } else {
-                AsyncTask.execute(new Runnable() {
-                    @Override
-                    public void run() {
-
-                        if (!sketchView.isNull()) {
-
-                            File root = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), "SketchIt");
-                            if (!root.exists()) root.mkdirs();
-
-                            SharedPreferences sharedPreferences = getSharedPreferences("phone.vishnu.statussaver", Context.MODE_PRIVATE);
-                            int lastInt = (sharedPreferences.getInt("number", 0)) + 1;
-                            String file = root.toString() + File.separator + "SketchIt" + lastInt + ".jpg";
-                            SharedPreferences.Editor editor = sharedPreferences.edit();
-                            editor.putInt("number", lastInt);
-                            editor.apply();
-
-                            Bitmap result = Bitmap.createBitmap(sketchView.getWidth(), sketchView.getHeight(), Bitmap.Config.ARGB_8888);
-                            Canvas c = new Canvas(result);
-                            sketchView.draw(c);
-
-                            try {
-                                FileOutputStream fOutputStream = new FileOutputStream(file);
-                                final BufferedOutputStream bos = new BufferedOutputStream(fOutputStream);
-
-                                result.compress(Bitmap.CompressFormat.JPEG, 90, bos);
-
-                                fOutputStream.flush();
-                                fOutputStream.close();
-
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                            MediaScannerConnection.scanFile(MainActivity.this, new String[]{file}, null, null);
-
-                        }
-                    }
-                });
+                return true;
             }
         }
+        return false;
     }
+
+    private void showPermissionDeniedDialog() {
+
+        final AlertDialog.Builder builder =
+                new AlertDialog.Builder(this);
+        builder.setTitle("Permission Denied");
+        builder.setMessage("Please Accept Permission");
+        builder.setCancelable(true);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+                int PERMISSION_REQ_CODE = 2;
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQ_CODE);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
+
+    }
+
 }
 
